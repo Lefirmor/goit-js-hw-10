079c273c9a8fdf93021d48feca5f293413e949ea
Timofey_Slakva <111429897+Lefirmor@users.noreply.github.com>
2023-02-21 23:40:20 +0300

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import debounce from 'lodash.debounce';
 import fetchCountries from './fetch-countries';
 import './css/styles.css';
+import { Notify } from 'notiflix';
+import renderCountries from './render-country';
 
 const DEBOUNCE_DELAY = 300;
 
-
-const inputRef = document.querySelector('#search-box')
-
-inputRef.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY))
+const inputRef = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list')
+const countryInfoRef = document.querySelector('.country-info')
+inputRef.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
 
 function searchCountries(e) {
-    let getCountry = e.target.value.trim();
+  let getCountry = e.target.value.trim();
+  if (getCountry) {
     fetchCountries(getCountry)
-        .then(data => {
-            console.log(data)
-        })
-        .catch(error => {
-            console.log("error")
-        })
+      .then(data => {
+        renderCountries(data)
+      })
+      .catch(error => {
+        Notify.failure('there is no such country');
+      });
+    
+  }
+  else{
+    countryListRef.innerHTML = "";
+    countryInfoRef.innerHTML = "";
+  }
 }
---
+++ src/render-country.js
@@ -0,0 +1,62 @@
+import { Notify } from 'notiflix';
+
+export default function renderCountries(dataCountries) {
+  const countryListRef = document.querySelector('.country-list');
+  const countryInfoRef = document.querySelector('.country-info');
+
+  let countries = [];
+
+  let quantityOfCountries = dataCountries.length;
+  if(quantityOfCountries > 10){
+    Notify.info("Too many matches found. Please enter a more specific name.")
+  }
+
+  else if(quantityOfCountries > 2 && quantityOfCountries <= 10){
+     dataCountries.map(country =>{
+        countries += `
+            <li>
+                <img src = "${country.flags.svg}" class = "flag" width = 30/>
+                <span class = "country-name">${country.name}</span>
+            </li>
+        `
+        countryListRef.innerHTML = countries;
+    })
+  }
+
+  else{
+        let country = dataCountries[0]
+        let languages = []
+
+    country.languages.map(language =>{
+        languages.push(language.name)
+    })
+
+    countryInfoRef.innerHTML = `
+        
+        <div class = "country-header"
+            <img src = "${country.flags.svg}" class = "flag" width = 30/>
+            <h2 class = "title">${country.name}</h2>
+        </div>
+
+        <ul class = "country-parameters"
+            <li class = "country-parameters__parameter"> 
+                <h2 class = "title">Capital:</h2>
+                <span>${country.capital}</span>
+            </li>
+            <li class = "country-parameters__parameter"> 
+                <h2 class = "title">Population:</h2>
+                <span>${country.population}</span>
+            </li>
+            
+            <li class = "country-parameters__parameter"> 
+                <h2 class = "title">Languages:</h2>
+                <span>${languages}</span>
+            </li>
+            
+        </ul>
+    `
+    countryListRef.innerHTML = ""
+  }
+
+  
+}
